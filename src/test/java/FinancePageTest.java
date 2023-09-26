ae4f7d6238f282ddca5e31652536167d2333cd1b
Michael Montgomery <michael.montgomery@tala.co>
2023-09-26 14:28:50 -0700

    small update

--- src/test/java/FinancePageTest.java
+++ src/test/java/FinancePageTest.java
@@ -1,6 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.FinanceConstants;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ public class FinancePageTest extends BaseTest implements FinanceConstants {
         Assert.assertNotNull(pageTitle);
     }
 
-    @Test
+    @Test //Added extra test here that was not in the prompt
     public void testAssertFinancePageTitleIsCorrect(){
         var pageTitle = financePage.getPageTitle();
         Assert.assertEquals(pageTitle, PAGETITLE);
@@ -20,20 +21,26 @@ public class FinancePageTest extends BaseTest implements FinanceConstants {
 
     @Test
     public void testCompareFinancePageStockSymbols(){
-        var pageStockSymbols = financePage.getInterestedFinancePageStockSymbols();
-        List<String> foundOnBoth = new ArrayList<>();
+        var stockSymbolsOnGoogle = financePage.getInterestedFinancePageStockSymbols();
+        System.out.println("LISTS");
+        System.out.println("our list ------ " + EXPECTEDSTOCKSYMBOLS);
+        System.out.println("google list --- " + stockSymbolsOnGoogle);
         List<String> foundOnlyOnGoogle = new ArrayList<>();
-        for (String symbol : pageStockSymbols) {
-            if (EXPECTEDSTOCKSYMBOLS.contains(symbol)){
-                foundOnBoth.add(symbol);
-            } else {
+        List<String> foundOnlyInExpectedStockSymbols = new ArrayList<>();
+        for (String symbol : stockSymbolsOnGoogle) {
+            if (!EXPECTEDSTOCKSYMBOLS.contains(symbol)){
                 foundOnlyOnGoogle.add(symbol);
             }
         }
-        System.out.println("Found on Both Google Finance Page and in our expected list");
-        System.out.println(foundOnBoth);
-        System.out.println("==========================================================");
-        System.out.println("Found ONLY on Google Finance");
+        for (String symbol : EXPECTEDSTOCKSYMBOLS) {
+            if (!stockSymbolsOnGoogle.contains(symbol)){
+                foundOnlyInExpectedStockSymbols.add(symbol);
+            }
+        }
+        System.out.println("Found only on Google Finance");
         System.out.println(foundOnlyOnGoogle);
+        System.out.println("==========================================================");
+        System.out.println("Found only in expected list");
+        System.out.println(foundOnlyInExpectedStockSymbols);
     }
 }
